1f5c7edacdfc404d292637d7df068fda35b5464c
AvishkaEJ <jaysthlaka@gmail.com>
2025-02-26 15:59:11 +0530

    Daily record done

--- src/pages/intern/DailyUpdates.jsx
+++ src/pages/intern/DailyUpdates.jsx
@@ -42,142 +42,167 @@ const ProjectCard = () => {
     const allDates = Object.keys(attendance).sort(); // Sorted for consistency
 
   return (
-    <div className='m-6'>
-      <Card className='p-4 w-full'>
+    <div className="m-6">
+      <Card className="p-4 w-full">
         <CardHeader>
-            <h1 className='font-bold text-lg text-blue'>JAVA8 - Intern Management System</h1>
+          <h1 className="font-bold text-lg text-blue">
+            JAVA8 - Intern Management System
+          </h1>
         </CardHeader>
         <CardBody>
-            <div className="w-full space-y-6 mt-4">
-                <div className="flex flex-wrap md:flex-nowrap gap-6">
-                    <Input 
-                        isDisabled
-                        size='md' 
-                        variant='bordered' 
-                        label="Group Name" 
-                        labelPlacement='outside' 
-                        type="text" 
-                        placeholder='JAVA8' 
-                        className="w-full md:w-1/2"
-                    />
+          <div className="w-full space-y-6 mt-4">
+            <div className="flex flex-wrap md:flex-nowrap gap-6">
+              <Input
+                isDisabled
+                size="md"
+                variant="bordered"
+                label="Group Name"
+                labelPlacement="outside"
+                type="text"
+                placeholder="JAVA8"
+                className="w-full md:w-1/2"
+              />
 
-                    <Input 
-                        size='md' 
-                        isDisabled
-                        variant='bordered' 
-                        label="Project Name" 
-                        labelPlacement='outside' 
-                        type="text" 
-                        placeholder='Intern Management System' 
-                        className="w-full md:w-1/2"
-                    />
-                </div>
-
-                <div>
-                    <Textarea  
-                        variant='bordered' 
-                        isDisabled
-                        label="Description" 
-                        labelPlacement='outside' 
-                        type="text" 
-                        placeholder='The description about the Intern Management System' 
-                        className="w-full"
-                    />
-                </div>
-                <div className="flex flex-wrap md:flex-nowrap gap-6">
-                    <Input 
-                        isDisabled
-                        size='md' 
-                        variant='bordered' 
-                        label="Supervisor" 
-                        labelPlacement='outside' 
-                        type="text" 
-                        placeholder='M.Giridaran' 
-                        className="w-full md:w-1/2"
-                    />
-
-                </div>
-
-                <div className="flex flex-wrap md:flex-nowrap gap-6">
-                    <DatePicker 
-                        variant='bordered' 
-                        isDisabled
-                        label="Start Date" 
-                        labelPlacement='outside' 
-                        defaultValue={parseDate("2024-04-04")}
-                        className="w-full md:w-1/2"
-                    />
-
-                    <DatePicker 
-                        variant='bordered' 
-                        isDisabled
-                        label="Target Date" 
-                        labelPlacement='outside' 
-                        defaultValue={parseDate("2024-04-04")}
-                        className="w-full md:w-1/2"
-                    />
-                </div>
-
-                {/* Past Attendance Table */}
-                <div className="mt-20">
-                    <div className='flex justify-between items-center mt-10 mb-3'>
-                        <h2 className="font-semibold text-md mb-2">Daily records</h2>
-
-                        {/* Button to Open Daily records Modal */}
-                        <div className="flex flex-wrap md:flex-nowrap gap-6">
-                        <Button className="font-bold text-white bg-blue " onClick={() => setIsModalOpen(true)}>
-                                Viwe Records
-                            </Button>
-                            <Button className="font-bold text-white bg-green" onClick={() => setIsModalOpen(true)}>
-                                Add Daily Record
-                            </Button>
-                        </div>
-                    </div>
-                </div>
+              <Input
+                size="md"
+                isDisabled
+                variant="bordered"
+                label="Project Name"
+                labelPlacement="outside"
+                type="text"
+                placeholder="Intern Management System"
+                className="w-full md:w-1/2"
+              />
             </div>
+
+            <div>
+              <Textarea
+                variant="bordered"
+                isDisabled
+                label="Description"
+                labelPlacement="outside"
+                type="text"
+                placeholder="The description about the Intern Management System"
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-wrap md:flex-nowrap gap-6">
+              <Input
+                isDisabled
+                size="md"
+                variant="bordered"
+                label="Supervisor"
+                labelPlacement="outside"
+                type="text"
+                placeholder="M.Giridaran"
+                className="w-full md:w-1/2"
+              />
+            </div>
+
+            <div className="flex flex-wrap md:flex-nowrap gap-6">
+              <DatePicker
+                variant="bordered"
+                isDisabled
+                label="Start Date"
+                labelPlacement="outside"
+                defaultValue={parseDate("2024-04-04")}
+                className="w-full md:w-1/2"
+              />
+
+              <DatePicker
+                variant="bordered"
+                isDisabled
+                label="Target Date"
+                labelPlacement="outside"
+                defaultValue={parseDate("2024-04-04")}
+                className="w-full md:w-1/2"
+              />
+            </div>
+            <Table>
+              <TableHeader>
+                <TableColumn>Team Member Name</TableColumn>
+              </TableHeader>
+              <TableBody>
+                {members.map((member) => (
+                  <TableRow key={member.id}>
+                    <TableCell>{member.name}</TableCell>
+                    {allDates.map((date) => (
+                      <TableCell key={`${date}-${member.id}`}>
+                        {attendance[date]?.[member.id] ? "✅" : "❌"}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+            {/* Past Attendance Table */}
+            <div className="mt-20">
+              <div className="flex justify-between items-center mt-10 mb-3">
+                <h2 className="font-semibold text-md mb-2">Daily records</h2>
+
+                {/* Button to Open Daily records Modal */}
+                <div className="flex flex-wrap md:flex-nowrap gap-6">
+                  <Button
+                    className="font-bold text-white bg-blue "
+                    onClick={() => setIsModalOpen(true)}
+                  >
+                    Viwe Records
+                  </Button>
+                  <Button
+                    className="font-bold text-white bg-green"
+                    onClick={() => setIsModalOpen(true)}
+                  >
+                    Add Daily Record
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
         </CardBody>
 
         {/* Record Marking Modal */}
         <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                <ModalContent>
-                    <ModalHeader>Add daily record</ModalHeader>
-                    <ModalBody>
-                        
-                        {/* Select Date */}
-                        <DatePicker 
-                            variant='bordered' 
-                            label="Select the Date" 
-                            labelPlacement='outside' 
-                            selected={selectedDate}
-                            onChange={setSelectedDate}
-                            className="w-full"
-                        />
+          <ModalContent>
+            <ModalHeader>Add daily record</ModalHeader>
+            <ModalBody>
+              {/* Select Date */}
+              <DatePicker
+                variant="bordered"
+                label="Select the Date"
+                labelPlacement="outside"
+                selected={selectedDate}
+                onChange={setSelectedDate}
+                className="w-full"
+              />
 
-                        {/* text area */}
-                        <Textarea  
-                        variant='bordered' 
-                        label="Daily progress" 
-                        labelPlacement='outside' 
-                        type="text" 
-                        placeholder='Enter your progress' 
-                        className="w-full"
-                    />
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="danger" variant="light" onClick={() => setIsModalOpen(false)}>
-                            Cancel
-                        </Button>
-                        <Button color="primary" onClick={() => setIsModalOpen(false)}>
-                            Save Recoed
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-    </Card>
-
+              {/* text area */}
+              <Textarea
+                variant="bordered"
+                label="Daily progress"
+                labelPlacement="outside"
+                type="text"
+                placeholder="Enter your progress"
+                className="w-full"
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                color="danger"
+                variant="light"
+                onClick={() => setIsModalOpen(false)}
+              >
+                Cancel
+              </Button>
+              <Button color="primary" onClick={() => setIsModalOpen(false)}>
+                Save Recoed
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Card>
     </div>
-    
-  )
+  );
 }
 
 export default ProjectCard
